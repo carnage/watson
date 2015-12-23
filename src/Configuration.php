202753b35e2cb44d38842630a7a135930855f73b
Carnage <t.carnage@gmail.com>
2015-12-23 19:10:05 +0000

    Minor tweaks to improve ability to integrate with Zf2

--- src/Configuration.php
+++ src/Configuration.php
@@ -4,18 +4,39 @@ namespace Carnage\Watson;
 
 use Carnage\Watson\Logger\LoggerInterface;
 use Doctrine\Common\Cache\ArrayCache;
+use Carnage\Watson\Walker\SqlWalker;
+use Doctrine\DBAL\Logging\SQLLogger;
 use Doctrine\ORM\Configuration as DoctrineConfig;
+use Doctrine\ORM\Query;
 
 class Configuration extends DoctrineConfig
 {
+    const HYDRATE_OBJECT = 'watson-hydrate-object';
+    const HYDRATE_ARRAY = 'watson-hydrate-array';
+    const HYDRATE_SCALAR = 'watson-hydrate-scalar';
+    const HYDRATE_SINGLE_SCALAR = 'watson-hydrate-single-scalar';
+    const HYDRATE_SIMPLEOBJECT = 'watson-hydrate-simple-object';
+
     public function __construct(DoctrineConfig $config)
     {
         $this->_attributes = $config->_attributes;
+
+        $this->setDefaultQueryHint(Query::HINT_CUSTOM_OUTPUT_WALKER, SqlWalker::class);
+        $this->setCustomHydrationModes([
+            static::HYDRATE_OBJECT => Hydrator\ObjectHydrator::class,
+            static::HYDRATE_ARRAY => Hydrator\ArrayHydrator::class,
+            static::HYDRATE_SCALAR => Hydrator\ScalarHydrator::class,
+            static::HYDRATE_SINGLE_SCALAR => Hydrator\SingleScalarHydrator::class,
+            static::HYDRATE_SIMPLEOBJECT => Hydrator\SimpleObjectHydrator::class
+        ]);
+
         $this->_attributes['addQueryComment'] = true;
     }
 
     /**
      * Caching query's messes with logging their source, this prevents query caching
+     *
+     * @TODO maybe do something about trashing people's configs
      *
      * @return ArrayCache
      */
@@ -24,9 +45,19 @@ class Configuration extends DoctrineConfig
         return new ArrayCache();
     }
 
+    /**
+     * @TODO write code to prevent trashing of existing log setup
+     * @param LoggerInterface $logger
+     */
     public function setWatsonLogger(LoggerInterface $logger)
     {
         $this->_attributes['watsonLogger'] = $logger;
+        $this->setSQLLogger($logger);
+    }
+
+    public function setSQLLogger(SQLLogger $logger = null)
+    {
+        null;
     }
 
     /**
--- src/Walker/SqlWalker.php
+++ src/Walker/SqlWalker.php
@@ -2,9 +2,9 @@
 
 namespace Carnage\Watson\Walker;
 
+use Carnage\Watson\Configuration;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\Query\SqlWalker as BaseWalker;
-use Carnage\Watson\Watson;
 
 class SqlWalker extends BaseWalker
 {
@@ -52,19 +52,19 @@ class SqlWalker extends BaseWalker
         $mode = $this->getQuery()->getHydrationMode();
         switch ($mode) {
             case Query::HYDRATE_OBJECT:
-                $this->getQuery()->setHydrationMode(Watson::HYDRATE_OBJECT);
+                $this->getQuery()->setHydrationMode(Configuration::HYDRATE_OBJECT);
                 break;
             case Query::HYDRATE_ARRAY:
-                $this->getQuery()->setHydrationMode(Watson::HYDRATE_ARRAY);
+                $this->getQuery()->setHydrationMode(Configuration::HYDRATE_ARRAY);
                 break;
             case Query::HYDRATE_SCALAR:
-                $this->getQuery()->setHydrationMode(Watson::HYDRATE_SCALAR);
+                $this->getQuery()->setHydrationMode(Configuration::HYDRATE_SCALAR);
                 break;
             case Query::HYDRATE_SINGLE_SCALAR:
-                $this->getQuery()->setHydrationMode(Watson::HYDRATE_SINGLE_SCALAR);
+                $this->getQuery()->setHydrationMode(Configuration::HYDRATE_SINGLE_SCALAR);
                 break;
             case Query::HYDRATE_SIMPLEOBJECT:
-                $this->getQuery()->setHydrationMode(Watson::HYDRATE_SIMPLEOBJECT);
+                $this->getQuery()->setHydrationMode(Configuration::HYDRATE_SIMPLEOBJECT);
                 break;
         }
     }
